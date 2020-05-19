06d267aa6261b02d2d337c81e3402dec53500b04
bugu <buguing@gmail.com>
2020-05-19 14:12:56 +0800

    first commit

---
+++ app/src/main/java/com/wellee/coroutine/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.wellee.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        GlobalScope.launch(Dispatchers.Main) {
+            ioFun1()
+            uiFun1()
+            ioFun2()
+            uiFun2()
+            ioFun3()
+            uiFun3()
+        }
+        println("main : current thread name == ${Thread.currentThread().name}")
+    }
+
+    private suspend fun ioFun1() {
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+            println("ioFun1 : current thread name == ${Thread.currentThread().name}")
+        }
+    }
+
+    private suspend fun ioFun2() {
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+            println("ioFun2 : current thread name == ${Thread.currentThread().name}")
+        }
+    }
+
+    private suspend fun ioFun3() {
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+            println("ioFun3 : current thread name == ${Thread.currentThread().name}")
+        }
+    }
+
+    private fun uiFun1() {
+        println("uiFun1 : current thread name == ${Thread.currentThread().name}")
+    }
+
+    private fun uiFun2() {
+        println("uiFun2 : current thread name == ${Thread.currentThread().name}")
+    }
+
+    private fun uiFun3() {
+        println("uiFun3 : current thread name == ${Thread.currentThread().name}")
+    }
+}
